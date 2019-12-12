234a7f0ec41c994cb33caa9832ad37ea64e13139
Greg Lahaye <greg@lahayes.net>
2019-12-12 22:25:21 +0800

    Just needs some cleanup, testing and input/output

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
 class Period {
   constructor(day, start, end) {
     this.day = day;
-    this.start = this.parse(start);
-    this.end = this.parse(end);
+    this.start = Period.parse(start);
+    this.end = Period.parse(end);
     this.length = this.end - this.start;
   }
 
-  parse(s) {
+  static parse(s) {
     const parts = s.split(':');
     const hours = parseInt(parts[0], 10);
     const minutes = parseInt(parts[1], 10);
@@ -14,7 +14,8 @@ class Period {
   }
 
   overlaps(other) {
-    return Math.max(this.start, other.start) < Math.min(this.end, other.end);
+    return this.day === other.day
+        && Math.max(this.start, other.start) < Math.min(this.end, other.end);
   }
 }
 
@@ -25,21 +26,68 @@ class Lesson {
     this.period = new Period(day, start, end);
     this.venue = venue;
   }
+
+  same(other) {
+    return this.unit === other.unit && this.activity === other.activity;
+  }
 }
 
 class Schedule {
   constructor(lessons, travel) {
     this.lessons = lessons;
+    this.travel = travel;
     this.calculate();
   }
 
   calculate() {
-    // TODO: calculate score
-    this.score = travel;
+    this.score = 0;
+
+    const days = {};
+
+    this.lessons.forEach((lesson) => {
+      if (!(lesson.period.day in days)) {
+        days[lesson.period.day] = {
+          length: 0,
+          start: 24,
+          end: 0,
+        };
+      }
+
+      days[lesson.period.day].length += lesson.period.length;
+
+      if (lesson.period.start < days[lesson.period.day].start) {
+        days[lesson.period.day].start = lesson.period.start;
+      }
+
+      if (lesson.period.end > days[lesson.period.day].end) {
+        days[lesson.period.day].end = lesson.period.end;
+      }
+    });
+
+    this.score = (this.travel * 2) * Object.keys(days).length;
+
+    Object.keys(days).forEach((day) => {
+      this.score += days[day].end - days[day].start - days[day].length;
+    });
   }
 
   conflicts() {
-    // TODO: check if lessons conflict
+    let conflict = false;
+
+    const periods = [];
+    for (let i = 0; i < this.lessons.length - 1; i += 1) {
+      for (let j = i + 1; j < this.lessons.length; j += 1) {
+        periods.push([this.lessons[i].period, this.lessons[j].period]);
+      }
+    }
+
+    let i = 0;
+    while (!conflict && i < periods.length) {
+      conflict = periods[i][0].overlaps(periods[i][1]);
+      i += 1;
+    }
+
+    return conflict;
   }
 }
 
@@ -90,22 +138,42 @@ function lessonFilter(lesson, blocked) {
   // filter out lessons during blocked periods
   let i = 0;
   while (valid && i < blocked.length) {
-    valid = !lesson.overlaps(blocked[i]);
+    valid = !lesson.period.overlaps(blocked[i]);
     i += 1;
   }
 
   return valid;
 }
 
-function findBestSchedules(lessons) {
-  const schedules = [];
+function findBestSchedules(lessons, travelTime) {
+  const groups = [];
 
-  schedules.push(new Schedule(lessons[0])); // this don't make sense
+  lessons.forEach((lesson) => {
+    let found = false;
+    let i = 0;
+    while (!found && i < groups.length) {
+      found = groups[i][0].same(lesson);
+      i += 1;
+    }
+
+    if (found) {
+      groups[i - 1].push(lesson);
+    } else {
+      groups.push([lesson]);
+    }
+  });
 
   // create all permuations
-  // create Schedule object of each permuation
-  // remove schedules that conflicts()
-  // calculate score of each schedule
+  // https://stackoverflow.com/questions/12303989/cartesian-product-of-multiple-arrays-in-javascript/50631472#50631472
+  const permutations = groups.reduce((acc, curr) => acc.flatMap((c) => curr.map((n) => [].concat(c, n))));
+
+  const schedules = [];
+  permutations.forEach((permutation) => {
+    const schedule = new Schedule(permutation, travelTime);
+    if (!schedule.conflicts()) {
+      schedules.push(schedule);
+    }
+  });
 
   return schedules;
 }
@@ -113,24 +181,28 @@ function findBestSchedules(lessons) {
 function f() {
   // user defined values
   const blocked = [
-    new Period('Monday', '14:00', '18:00'),
+    new Period('Monday', '16:00', '20:00'),
     new Period('Friday', '06:00', '18:00'),
     new Period('Thursday', '12:00', '14:00'),
   ];
-  const travelTime = 2;
+
+  const travelTime = 120;
 
   let lessons = parseDOM();
   lessons = lessons.filter((lesson) => lessonFilter(lesson, blocked));
 
   const schedules = findBestSchedules(lessons, travelTime);
 
-  console.log(schedules);
-}
+  let best = schedules[0];
 
-/*
- * UI Notes:
- * Blocked period inputs
- * Activate buttons
- */
+  schedules.forEach((schedule) => {
+    if (schedule.score < best) {
+      best = schedule;
+    }
+  });
+
+  console.log(schedules);
+  console.log(best);
+}
 
 f();
